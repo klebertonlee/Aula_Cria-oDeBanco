bf6dd5165552bdac9ffe5f7bb7b8bde17b6e80c2
Klberton <kpleandro@hotmail.com>
2020-06-21 14:16:53 -0300

    projeto de banco iniciado

---
+++ Aula/Colossos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula
+{
+    class Colossos
+    {
+        private string nome;
+        private int numConta;
+        private double deposito;
+
+        public Colossos(string nome, int numConta, double deposito)
+        {
+            Nome = nome;
+            NumConta = numConta;
+            Deposito = deposito;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public int NumConta
+        {
+            get { return numConta; }
+            set { numConta = value; }
+        }
+        public double Deposito
+        {
+            get { return deposito; }
+            set { deposito = value; }
+        }
+
+        
+
+
+        public override string ToString()
+        {
+            return "Conta: "
+                + numConta
+                + ", "
+                + "Titular: "
+                + nome
+                + ", "
+                + "Saldo: $ "
+                + deposito.ToString("F2");
+        }
+    }
+}
---
+++ Aula/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o número da Conta: ");
+            int NumConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o Titular da Conta: ");
+            string Nome = Console.ReadLine();
+            Console.Write("Hávera deposito inicial (S/N)? ");
+            char S = char.Parse(Console.ReadLine().ToUpper());
+
+            if ('N' != S )
+            {
+                Console.Write("Entre com o Valor a ser Depositado: ");
+            }
+            else
+            {
+                Console.WriteLine("Sem valor para Deposito");
+            }
+            
+            double Deposito = double.Parse(Console.ReadLine().ToString());
+            Console.WriteLine();
+
+
+            Colossos p = new Colossos(Nome, NumConta, Deposito);
+            Console.WriteLine("Dados da Conta: ");
+            Console.Write(p);
+
+        }
+    }
+}
